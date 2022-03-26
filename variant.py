6a4cabba38e830e4ca579b1472735fbac2a52345
Belzedar94 <54615238+Belzedar94@users.noreply.github.com>
2022-03-26 21:33:07 +0100

    Update variant.py

--- variant.py
+++ variant.py
@@ -2,21 +2,20 @@ RANKS = 8
 FILES = 8
 SQUARES = RANKS * FILES
 KING_SQUARES = RANKS * FILES
-PIECE_TYPES = 13
+PIECE_TYPES = 11
 PIECES = 2 * PIECE_TYPES
 USE_POCKETS = True
 POCKETS = 2 * FILES if USE_POCKETS else 0
 
 PIECE_VALUES = {
-    1 : 126,
-    2 : 781,
-    3 : 825,
-    4 : 1276,
-    5 : 2538,
-    6: 420,
-    7: 1800,
-    8: 2200,
-    9: 400,
-    10: 420,
-    11: 700,
+     1: 126,
+    2: 781,
+    3: 825,
+    4: 1276,
+    5: 2538,
+    6: 700,
+    7: 800,
+    8: 200,
+    9: 520,
+    10: 700,
 }
