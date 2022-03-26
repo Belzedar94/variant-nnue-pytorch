814c0ea6ef599ca7fe2ef0bfe4b93e45919e2356
Belzedar94 <54615238+Belzedar94@users.noreply.github.com>
2022-03-26 21:33:33 +0100

    Update nnue_training_data_formats.h

--- lib/nnue_training_data_formats.h
+++ lib/nnue_training_data_formats.h
@@ -56,9 +56,9 @@ THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #define FILES 8
 #define RANKS 8
-#define PIECE_TYPES 6
+#define PIECE_TYPES 11
 #define PIECE_COUNT 32
-#define POCKETS false
+#define POCKETS true
 #define KING_SQUARES FILES * RANKS
 
 namespace chess
