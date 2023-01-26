b0b9c5cc3e1970569620efb70c7057122167e056
Belzedar94 <54615238+Belzedar94@users.noreply.github.com>
2023-01-26 08:52:47 +0100

    Update nnue_training_data_formats.h

--- lib/nnue_training_data_formats.h
+++ lib/nnue_training_data_formats.h
@@ -54,12 +54,12 @@ THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include <intrin.h>
 #endif
 
-#define FILES 8
+#define FILES 10
 #define RANKS 8
-#define PIECE_TYPES 6
-#define PIECE_COUNT 32
+#define PIECE_TYPES 7
+#define PIECE_COUNT 36
 #define POCKETS false
-#define KING_SQUARES 64
+#define KING_SQUARES 80
 
 namespace chess
 {
