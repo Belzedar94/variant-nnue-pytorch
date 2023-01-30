4123d30fb9cbc4b898b30ab9a337bd727f42bed7
Belzedar94 <54615238+Belzedar94@users.noreply.github.com>
2023-01-30 22:21:41 +0100

    Update nnue_training_data_formats.h

--- lib/nnue_training_data_formats.h
+++ lib/nnue_training_data_formats.h
@@ -54,13 +54,13 @@ THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include <intrin.h>
 #endif
 
-#define FILES 8
-#define RANKS 8
-#define PIECE_TYPES 6
-#define PIECE_COUNT 32
+#define FILES 12
+#define RANKS 10
+#define PIECE_TYPES 8
+#define PIECE_COUNT 44
 #define POCKETS false
-#define KING_SQUARES FILES * RANKS
-#define DATA_SIZE 512
+#define KING_SQUARES 120
+#define DATA_SIZE 1024
 
 static_assert(DATA_SIZE % 8 == 0);
 
