ee967024ad9db996db32da7122a79acdd4dc311a
Belzedar94 <54615238+Belzedar94@users.noreply.github.com>
2025-09-18 06:59:23 +0200

    Update variant.h

--- variant.h
+++ variant.h
@@ -1,7 +1,7 @@
-#define FILES 8
-#define RANKS 8
-#define PIECE_TYPES 8
-#define PIECE_COUNT 32
+#define FILES 5
+#define RANKS 6
+#define PIECE_TYPES 1
+#define PIECE_COUNT 30
 #define POCKETS false
 #define KING_SQUARES 1
 #define DATA_SIZE 512
